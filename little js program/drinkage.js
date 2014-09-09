abe0603c0bfe5e2b796e4c967d9cca13e320f8d5
leachy14 <Chris@Christophers-MacBook-Pro.local>
2014-09-08 20:38:26 -0400

    Small JS program
    I made this over the summer as one of my first programs and I’m testing
    it on github.

---
+++ little js program/drinkage.js
@@ -0,0 +1,8 @@
+alert("Im going tell you if you can drink in a bar or not, Please enter your age.");
+var age = prompt("What is your age?");
+
+if ( age < 21) {
+  alert("Sorry bud you can't drink");
+} else {
+  alert("Bottoms up mate!");
+}
