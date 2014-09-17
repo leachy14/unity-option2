3ade5b0a08043bd29c8e1aa318045f4738d7f7ce
kevinleary <kevin.n.leary@gmail.com>
2014-09-17 13:29:38 -0400

    Version1.1

---
+++ SchoolPlatformer/Assets/Scripts/camerascript.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var scrollSpeed: float;
+
+function Start () {
+
+}
+
+function Update () {
+	renderer.material.SetTextureOffset(
+	 "_MainTex",
+	 Vector2(scrollSpeed * Time.time,0)
+	 );
+
+}
