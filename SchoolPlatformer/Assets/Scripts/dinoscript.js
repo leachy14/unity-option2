323fbe56243480061f1ddbb1b43722924a2906b6
kevinleary <kevin.n.leary@gmail.com>
2014-09-29 20:50:12 -0400

    Version .13.3

---
+++ SchoolPlatformer/Assets/Scripts/dinoscript.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var dino: GameObject;
+var rngx: int;
+
+function Start () {
+
+	rngx = (Math.floor(Math.random() * 11);
+	var di: GameObject;
+	
+	di = Instantiate(
+		dino,
+		Vector3(rngx, -2.5,1),
+		
+
+function Update () {
+
+		var index = Mathf.FloorToInt(Time.time * 12.0) % 6;
+		var size = Vector2(0.15,1);
+		var offset = Vector2(index/6.0,0);
+		renderer.material.SetTextureScale("_MainTex", size);
+		renderer.material.SetTextureOffset("_MainTex", offset);
+
+}
