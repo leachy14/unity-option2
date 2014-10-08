f888de5ea80a782630c2bc1353bc38ecedc106ad
kevinleary <kevin.n.leary@gmail.com>
2014-10-08 13:04:24 -0400

    Version 0.15A

--- SchoolPlatformer/Assets/Scripts/dinomovescript.js
+++ SchoolPlatformer/Assets/Scripts/dinomovescript.js
@@ -1,14 +1,24 @@
 ﻿#pragma strict
 
 var dinospeed: float;
+var imageText: Texture;
+var dinoPrefab: Transform;
+var aniImage: Texture;
 
 function Start () {
 	
-	transform.Translate(dinospeed * Time.deltaTime,0,0); 
+	//transform.Translate(dinospeed * Time.deltaTime,0,0); 
 }
 
 function Update () {
 
-	if (transform.position.x)
+	renderer.material.mainTexture = aniImage;
 	transform.Translate(dinospeed * Time.deltaTime,0,0);
+	
+		var index = Mathf.FloorToInt(Time.time * 12.0) % 6;
+		var size = Vector2(0.15,1);
+		var offset = Vector2(index/6.0,0);
+		renderer.material.SetTextureScale("_MainTex", size);
+		renderer.material.SetTextureOffset("_MainTex", offset);
+		
 }
