db1c10e788e99463ad9e77a00878c66673875e52
Perry Berman <RealCloudCraft@users.noreply.github.com>
2014-10-15 11:06:47 -0400

    Version 0.15.0D
    Just touching this (i don't have the unity editor on the computer i wrote this on so i can't test it yet, going to test during period 9)
    
    
    the code I was testing is on the computer in the lab(The jump thing), I didn't put it up because there still is problems with it. (can jump an infinite amount of times.

--- SchoolPlatformer/Assets/Scripts/dinomovescript.js
+++ SchoolPlatformer/Assets/Scripts/dinomovescript.js
@@ -17,12 +17,12 @@ function Start () {
 
 function Update () {
 
-	renderer.material.mainTexture = aniImage;
-	transform.Translate(-dinospeed * Time.deltaTime,0,0);
 	
-		var index = Mathf.FloorToInt(Time.time * 12.0) % 6;
-		var size = Vector2(0.15,1);
-		var offset = Vector2(index/6.0,0);
+		renderer.material.mainTexture = aniImage;
+		transform.Translate(-dinospeed * Time.deltaTime,0,0);
+		index = Mathf.FloorToInt(Time.time * 12.0) % 6;
+		size = Vector2(0.15,1);
+		offset = Vector2(index/6.0,0);
 		renderer.material.SetTextureScale("_MainTex", size);
 		renderer.material.SetTextureOffset("_MainTex", offset);
 		
