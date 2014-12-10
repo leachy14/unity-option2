317d81c30983dd2f36d46a27997d418d7f59ce73
kevinleary <kevin.n.leary@gmail.com>
2014-12-09 20:39:32 -0500

    Version 0.03B
    December 9th - Still messing with different movement options for our
    enemy... ugh

---
+++ CandyDefense/Assets/Scripts/dotween.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+public class dotween : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		/*transform.DOPath (Vector3[] waypoints, 5, PathType pathType = Linear,
+		                  PathMode pathMode = 2D, 5, null);*/
+
+		//transform.DOMoveX (-2, 1);
+		//transform.DOMoveY (-2, -1).From(true);
+		//transform.DOMoveX (2, 1).From(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.DOMoveX (-2, 1);
+		transform.DOMoveY (-2, -1).From(true);
+	}
+}
