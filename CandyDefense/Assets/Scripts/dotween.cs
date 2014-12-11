a7ce1e15676af5eb319a9b1e96b2411371e455ff
kevinleary <kevin.n.leary@gmail.com>
2014-12-10 21:32:36 -0500

    Version 0.03C
    made some ground with the movement

---
+++ CandyDefense/Assets/Scripts/dotween.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class dotween : MonoBehaviour {
+
+	//public GameObject enemy_ph;
+	// Use this for initialization
+	void Start () {
+
+
+		transform.DOMoveX (-2, 2, false);
+
+
+
+		//transform.DOMove (new Vector2 (2, 2), 3, false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//for(int i = 1; i == 7; i++)
+
+
+		if (transform.position.x == -2 && transform.position.y == 0.75) {
+					transform.DOMoveY (-1.04f, 3, false);
+
+		}
+
+		if (transform.position.y == -1.04 && transform.position.x == -2) {
+						transform.DOMoveX (-1.069f, 2, false);
+					
+			}
+			
+	}
+}
