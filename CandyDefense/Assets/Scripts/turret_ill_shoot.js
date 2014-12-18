9645f4ff13ce4adf7bf9217c632ed413abb31cec
kevinleary <kevin.n.leary@gmail.com>
2014-12-18 13:13:33 -0500

    Version 0.1C
    December 18th - Perfected enemy path and started to implement illumanati
    turret

---
+++ CandyDefense/Assets/Scripts/turret_ill_shoot.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+
+function Start () {
+	
+	FindClosestEnemy();
+}
+
+function Update () {
+	
+	//if (enemy.gameObject 
+}
+
+function FindClosestEnemy (){
+		// Find all game objects with tag Enemy
+		var gos : GameObject[];
+		gos = GameObject.FindGameObjectsWithTag("Enemy"); 
+		var closest : GameObject; 
+		var distance = Mathf.Infinity; 
+		var position = transform.position; 
+		// Iterate through them and find the closest one
+		for (var go : GameObject in gos)  { 
+			var diff = (go.transform.position - position);
+			var curDistance = diff.sqrMagnitude; 
+			if (curDistance < distance) { 
+				closest = go; 
+				distance = curDistance; 
+			} 
+		} 	
