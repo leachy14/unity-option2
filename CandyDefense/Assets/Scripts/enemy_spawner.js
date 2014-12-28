8e10cbae669e4e55839f362c45fd362287e92497
Kevin Leary <kevin.n.leary@gmail.com>
2014-12-28 12:38:13 -0500

    Version 0.2C
    December 28th - Spawner now works and prefabs are spawning and following
    waypoints like a dream... turret stopped tracking the prefabs put im
    sure it is an easy fix

---
+++ CandyDefense/Assets/Scripts/enemy_spawner.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var dino: GameObject;
+var num = 5;
+var spawnTime: float = 5f;
+var spawnDelay: float = 3f;
+
+
+function Start () {
+	
+	InvokeRepeating("Spawn", spawnDelay, spawnTime); 
+	
+}
+
+
+
+function Update () {
+		
+}
+
+function Spawn() {
+	
+	//var enemyIndex = Random.Range(0, dino.length);
+	Instantiate(dino, transform.position, transform.rotation);
+	
+}
