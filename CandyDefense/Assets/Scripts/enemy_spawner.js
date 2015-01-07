d514d3acfb38ddbbf83a66bc07c5f94d90eba1dd
kevinleary <kevin.n.leary@gmail.com>
2015-01-07 08:41:27 -0500

    Please Sync github Perry to use latest commit
    Version 0.5B - went back to 0.4B script

--- CandyDefense/Assets/Scripts/enemy_spawner.js
+++ CandyDefense/Assets/Scripts/enemy_spawner.js
@@ -2,14 +2,14 @@
 
 var dino: GameObject;
 var num = 5;
-var spawnTime: float = 5f;
+//var spawnTime: float = 5f;
 var spawnDelay: float = 3f;
 
 
 function Start () {
 	
-	InvokeRepeating("Spawn", spawnDelay, spawnTime); 
-	
+	//InvokeRepeating("Spawn", spawnDelay, spawnTime); 
+	this.Spawn();
 }
 
 
@@ -20,7 +20,9 @@ function Update () {
 
 function Spawn() {
 	
-	//var enemyIndex = Random.Range(0, dino.length);
-	Instantiate(dino, transform.position, transform.rotation);
 	
+	for(var i = 1; i <= num; i++){//var enemyIndex = Random.Range(0, dino.length);
+	Instantiate(dino, transform.position, transform.rotation);
+	yield WaitForSeconds(spawnDelay);
+	}
 }
