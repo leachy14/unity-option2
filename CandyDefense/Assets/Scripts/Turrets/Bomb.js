4e8a5954feb49012c8f62da478ceea03454b0cfc
RealCloudCraft <option2@hsstudiobmac03.rtnj.local>
2015-02-19 11:59:42 -0500

    YAY The Animations aren't working

--- CandyDefense/Assets/Scripts/Turrets/Bomb.js
+++ CandyDefense/Assets/Scripts/Turrets/Bomb.js
@@ -1,26 +1,34 @@
 #pragma strict
 
 
+var Bomb : GameObject;
+var turret : GameObject;
+var gos : GameObject[];	
 var fireRate : int;
 private var nextFire = 0.0;
 
 // Use this for initialization
 function Start () {
 
-transform.Translate(0, 0, 0.2);
+animation.CrossFade("Countdown");
+rigidbody2D.velocity = transform.up * 0.1;
+FindClosestEnemy();	
 }
 
 // Update is called once per frame
 function Update () {
 
-
+		FindClosestEnemy();	
+		transform.position.z = 0;
+		transform.rotation.x = 0;
+		transform.rotation.y = 0;
 
 }
 function FindClosestEnemy () {
 		// Find all game objects with tag Enemy
 		
 		gos = GameObject.FindGameObjectsWithTag("Enemy"); 
-		turret = GameObject.FindGameObjectWithTag("Turret");
+		Bomb = GameObject.FindGameObjectWithTag("Projectile");
 		var closest : GameObject; 
 		var distance = Mathf.Infinity; 
 		var position = transform.position; 
@@ -34,10 +42,10 @@ function FindClosestEnemy () {
 				distance = curDistance; 
 			}
 			if (distance < 1 && closest.transform.position.x >= -2.388) {
-			transform.LookAt(turret.gameObject.FindGameObjectWithTag("Enemy").transform.position, Vector3.forward);
+			transform.LookAt(Bomb.gameObject.FindGameObjectWithTag("Enemy").transform.position, Vector3.forward);
 			if (Time.time > nextFire) {
 			nextFire = (Time.time + fireRate);
-			Shoot();
+			Explode();
 			
 			}
 		} 	
@@ -46,6 +54,7 @@ function FindClosestEnemy () {
 
 function Explode () {
 
+animation.Play("Countdown", PlayMode.StopAll);
 	
 
 }
