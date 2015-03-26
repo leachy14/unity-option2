d1f2d14dc3dd39d9ab41f3eed9170f040c775eba
Perry Berman <superstargamesnetwork@gmail.com>
2015-03-26 08:49:04 -0400

    Revert "working on stuff"
    This reverts commit d94b11b9a95f2589fcf27eb62d43391995a93c52.

--- CandyDefense/Assets/Scripts/LevelControl/Enemy_health_slider.cs
+++ CandyDefense/Assets/Scripts/LevelControl/Enemy_health_slider.cs
@@ -5,18 +5,15 @@ public class Enemy_health_slider : MonoBehaviour {
 
 
 	public Camera main;
-	public GameObject Enemy_for_this;
 	// Use this for initialization
 	void Start () {
-		Enemy_for_this = GameObject.Find ("Dino_enemy(Clone)");
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Enemy_for_this = GameObject.Find ("Dino_enemy(Clone)");
-		Vector3 vec = main.ScreenToWorldPoint(Enemy_for_this.transform.position);
-
-		transform.localPosition = new Vector3(vec.x, vec.y, -6f);
-		//transform.Translate
-	}	
+		Vector3 vec = main.ScreenToWorldPoint(GameObject.Find("Dino_enemy(Clone)").transform.position);
+		transform.position = vec;
+		transform.localPosition = new Vector2 (0, -0.1f);
+	}
 }
