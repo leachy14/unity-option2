55f98dcfb812c11460a99c054d6aa6c522626130
RealCloudCraft <option2@hsstudiobmac03.rtnj.local>
2015-04-08 14:03:56 -0400

    Working on health bar
    again….

--- CandyDefense/Assets/Scripts/LevelControl/Enemy_health_slider.cs
+++ CandyDefense/Assets/Scripts/LevelControl/Enemy_health_slider.cs
@@ -8,25 +8,33 @@ public class Enemy_health_slider : MonoBehaviour {
 	public Transform Enemy_for_this;
 	public Vector3 vec;	
 	public GameObject Bar;
-
+	public GameObject[] Dinos;
+	public GameObject[] Bars;
+	public int Hi = 0;
 	// Use this for initialization
 	void Start () {
-		//Enemy_for_this = GameObject.Find ("Dino_enemy(Clone)");
-		Debug.Log(transform.localToWorldMatrix);
-		Enemy_for_this = transform.Find ("/Dino_enemy(Clone)");
-		//Enemy_for_this = transform.Find ("/look for");
 
-		Bar.transform.parent = (GameObject.Find("HealthCoinGroup")).transform;
+		Debug.Log(transform.localToWorldMatrix);
+		Dinos = GameObject.FindGameObjectsWithTag("Enemy");
+		//Bars = GameObject.FindGameObjectsWithTag("Bar");
+
+		//Enemy_for_this = transform.Find ("/Dino_enemy(Clone)");
+		Enemy_for_this = Dinos[Dinos.Length].transform;
+		Bar = GameObject.Find ("HealthCoinGroup");
+		//Bar.transform.parent = transform.Find("/Canvas/HealthCoinGroup");
+		transform.SetParent(transform.Find("/Canvas"));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//Enemy_for_this = transform.Find ("/Dino_enemy(Clone)");
-		//Enemy_for_this = transform.Find ("/look for");
+		if (Hi < 1) {
+			//Dinos = GameObject.FindGameObjectsWithTag("Enemy");
+			Enemy_for_this = Dinos[Dinos.Length].transform;
+			Hi = Hi + 1;
+		}
+		Hi = Hi + 1;
 		vec = Enemy_for_this.InverseTransformDirection(Enemy_for_this.position.x,Enemy_for_this.position.y + -.25f, -6 );
 		transform.position = vec;
-				Debug.Log(vec);
-
 		}
 
 	}	
